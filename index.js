5aa1992308d3a8ed513941ae7b75170eba58625a
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-12 16:00:31 +0300

    "Updated the prependCat function's name to *yes*"

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(me){
+    cats.push(me);
+}
+function destructivelyPrependCat(keith){
+    cats.unshift(keith);
+}
+
+console.log(cats);
+
+// Function removes the last cat from the list
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(no) {
+
+    return [...cats, no];
+  }
+
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+  }
+
+  function removeFirstCat() {
+
+    return cats.slice(1);
+  }
+
+  function prependCat(yes) {
+
+    return [yes,...cats];
+  }
+
+  function removeLastCat() {
+    
+    return cats.slice(0, -1);
+  }
